b68361c320a86f83f5c8f9fec4c281fc2ab74e98
vkm94 <varunmanila89@gmail.com>
2022-02-17 23:25:51 +0530

    updated

--- admin/ajax.php
+++ admin/ajax.php
@@ -67,7 +67,7 @@ if($action == "delete_vacancy"){
 }
 if($action == "save_application"){
 	
-	$save = $crud->save_application();
+	$save = $crud->save_application($_POST);
 	
 	if($save)
 		echo $save;
