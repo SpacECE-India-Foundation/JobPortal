aaab2f5130ac3e0a92f06d22e7432661a8a7823b
vkm94 <varunmanila89@gmail.com>
2022-02-18 09:56:06 +0530

    updated

--- submit_application.php
+++ submit_application.php
@@ -125,7 +125,7 @@ $(document).ready(function(){
 		
 		e.preventDefault()
 		//start_load()
-		
+		alert( new FormData($(this)[0]));
 		$.ajax({
 			url:'admin/ajax.php?action=save_application',
 			data: new FormData($(this)[0]),
